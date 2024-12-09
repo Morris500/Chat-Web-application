34d4c192f1172f773328777bf332980b56334297
morris <akunnemorris@gmail.com>
2024-12-09 17:07:33 +0100

    creatachannel

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Channel, useChatContext } from 'stream-chat-react';
+import {UserList} from './'
+import {CloseCreateChannel} from '../assets'
 
-const CreateChannel = () => {
+
+const ChannelNameInput = ({channelName='', setChannelName}) => {
+  const handelchange = (e) =>{
+    e.preventDefault();
+    setChannelName(e.target.value);
+  }
   return (
-    <div>CreateChannel</div>
+    <div className='channel-name-input__wrapper'>
+      <p>Name</p>
+      <input value={channelName} onChange={handelchange} placeholder='channnel-name (no spaces)' />
+      <p>Add Member</p>
+    </div>
+  )
+}
+
+const CreateChannel = ({createType, setIscreating}) => {
+  return(
+    <div className='create-channel__container'>
+      <div className='create-channel__header'>
+        <p>{createType === 'team' ? 'create a New channel' : 'send a Direct Message'}</p>
+      </div>
+      <ChannelNameInput />
+    </div>
   )
 }
 
